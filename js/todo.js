6b791e2f8ba333debfaafc543678bc920cb75bdc
YUJIN JI <yuzzzin@gmail.com>
2023-07-13 17:29:04 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -5,12 +5,8 @@ const toDoList = document.querySelector('.todo-list');
 const TODOS_KEYS = 'todos';
 
 // ✨ 원하는 것 : paintToDo(newToDo); 가 될때 마다 그 텍스트를 array에 push 하고 싶다~
-const toDos = []; // todo를 담을 배열 생성
-
-// application이 시작 될 때, toDos arr는 항상 비어있어서 +
-// (js:62) toDos.push(newToDo); 작성하고 form을 submit 할 때 마다 > 빈 arr에 그냥 push하게 되어서
-// function saveToDos(); 실행할 때 새로운 toDo들만 포함하고있는 array를 저장하고 있음.
-// 이러한 이유로 새로 저장할 때 마다 이전 저장한 것은 사라지고 새로 추가한것만 덮어씌워져서 저장되고있음
+// const toDos = []; // todo를 담을 배열 생성
+let toDos = [];
 
 // ToDo 목록을 local Storage에 저장
 function saveToDos() {
@@ -38,13 +34,22 @@ function deleteToDo(event) {
 
   // li 삭제
   li.remove();
+
+  // 우리가 클릭한 li.id와 다른 toDo는 list에 남겨두고싶어!
+  // toDos = toDos.filter((toDo) => toDo.id !== li.id);
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id)); //parseInt() 로 li.id 형변환
+  // 오 근데 지워도 local에 또 남아있음
+  // 👉 datatype때문임, typeof (toDos.id )== number / typeof (li.id) == string 😞
+  saveToDos();
 }
 
 // todo 를 그리는 역할
 function paintToDo(newToDo) {
-  const span = document.createElement('span');
-  span.innerText = newToDo;
   const li = document.createElement('li');
+  li.id = newToDo.id;
+  const span = document.createElement('span');
+  // span.innerText = newToDo;
+  span.innerText = newToDo.text; // object.text
   const btn = document.createElement('button');
   btn.innerText = '❌';
   btn.addEventListener('click', deleteToDo);
@@ -60,12 +65,17 @@ function handleToDoSubmit(event) {
   toDoInput.value = ''; // 엔터 후 빈칸 되도록 toDoInput.value에 빈값 넣어주기
   // 이후 toDoInput.value 가 비워졌다고 하서 newToDo의 값이 비워지는 것 아님!
 
-  // toDos에 newTodo를 넣어주는 것
-  toDos.push(newToDo);
+  // toDos에 newTodo를 넣어주는 것 → newToDoObj로 변경(id 부여)
+  const newToDoObj = {
+    text: newToDo,
+    id: Date.now(), // Date.now() == ms 를 반환 해 주는 메서드
+  };
+  toDos.push(newToDoObj);
 
   // newTodo를 paintToDo에 보냄 -> newTodo는 값이 비워지지 않은 상태의 값으로
   // 즉, 입력값을 받아서 비워지기 전에 paintToDo로 값을 보내서 함수를 실행토록함
-  paintToDo(newToDo);
+  // paintToDo(newToDo);
+  paintToDo(newToDoObj); // text였던 newToDo가 아닌 obj인 newToDoObj로 변경
 
   // ToDo 목록을 local Storage에 저장
   saveToDos();
@@ -81,13 +91,12 @@ toDoForm.addEventListener('submit', handleToDoSubmit);
 
 const savedToDos = localStorage.getItem(TODOS_KEYS);
 
-if (saveToDos !== null) {
+if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
-  console.log(parsedToDos);
+  toDos = parsedToDos;
   parsedToDos.forEach(paintToDo);
   // array.foreach는 받아온 array를 for 반복문 없이 item 하나씩 function에 넣을 수 있는 신기한 뇨슥
-  // parsedToDos 내부의 item들을 가지고, 그 item들을 나타내고 싶은 것()
-  // painToDo fucntion을 넣어주면 됨! (painToDo는 newToDo를 인자로 전달받고 있음)
+  // ✨✨ forEach 함수는 이 paintToDo를 parsedToDos 배열의 요소마다 실행해주고 있따아 ✨✨
 
   // (item) => console.log('Hello', item)
   // ==
@@ -95,3 +104,45 @@ if (saveToDos !== null) {
   //   console.log('Hello', item);
   // }
 }
+
+function sexyFilter(item) {
+  return item !== 3; // item이 3일 경우 false 반환, 요소가 3일 경우 3은 제외된다~
+  // The sexyFilter() should return true, if you want to keep this obj in your new array.
+  // If you return false, that item is not going to be including in your new array
+  // 새 obj에도 1, 2, 3, 4 포함하고 싶으면! false 리턴하면 그 item은 새 arr에 포함 안됨
+}
+[1, 2, 3, 4].filter(sexyFilter); // filter가 sexyFilter에 1, 2, 3, 4를 차례대로 넣어서 부를거야
+// filter()
+// 원하는 요소만 가지고 새 arr 만들긔 (item을 지우는 것이 아니라 item을 '제외'하는 것!)
+
+//📌
+// application이 시작 될 때, toDos arr는 항상 비어있어서 +
+// (js:62) toDos.push(newToDo); 작성하고 form을 submit 할 때 마다 > 빈 arr에 그냥 push하게 되어서
+// function saveToDos(); 실행할 때 새로운 toDo들만 포함하고있는 array를 저장하고 있음.
+// 이러한 이유로 새로 저장할 때 마다 이전 저장한 것은 사라지고 새로 추가한것만 덮어씌워져서 저장되고있음
+// == 우리가 갖고있던 toDos의 이전 복사본들은 잊어버리고 있다
+
+// so, const toDos = []; 를 let으로 변경해서 update 가능하도록 만들고,
+// localStorage에 toDo들이 있으면 toDos에 parsedToDos를 넣어 전에 있던 toDo를 복원하는 코드를 써주자 (toDos = parsedToDos;)
+
+//📌
+// ㅋㅋㅋㅋ근데 이제 새로고침해도 안날라가고 localStorage에 있는것도 화면에 잘 보이는데
+// delete btn 누르고 새로고침하면 다시 죽지도않고 살아서 돌아옴 → (localStorage 에선 안지워졌기때문)
+// del btn 누를때 == localStr도 같이 업데이트 되게 해보잣
+
+// local storage == toDos array 를 복사해두는 곳
+// toDos array !== local Storage
+
+//📌
+// todo들에게 ID 같은 것을 주자!
+// 같은 value를 가진 요소들이 있을 때, id로 각각의 li item을 구별하기 위해서!
+// ((ex. ['a', 'b', 'c', 'a']) 어떤 'a'가 지워졌는지 알 수 없음)
+
+//📌
+// const arr = [1, 2, 3, 4]
+
+// arr.filter(item => item > 2)  // [3, 4]
+// const newArr = arr.filter(item => item > 2)
+
+// arr  // 1, 2, 3, 4]  👉 filter는 기존 arr를 변경하지 않는다!
+// newArr  // (2) [3, 4]
